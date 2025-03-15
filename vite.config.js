1c7b4485d928c1bf2c7c6e58168732b5ae8b5142
NiceMidnight <2457855475@qq.com>
2025-03-15 20:17:58 +0800

    feat：修复空白。

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
 import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/repo-name/', // 替换为你的仓库名称
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
